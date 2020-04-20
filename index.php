4d83d2ed447b1b71a024a461e01c8419750167c1
Rakhalr72 <l.rakha77@gmail.com>
2020-04-20 18:50:44 +0700

    update index

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 server_key = "SB-Mid-server-Ccg5adUBZ9FTnUcuzaG-NEyT";
 
 $is_production = false;
-$api_url = $is_production ? 'https://app.midtrans.com/snap/v1/transactions' : 'https://api.sandbox.midtrans.com/snap/v1/transactions'; 
+$api_url = $is_production ? 'https://app.midtrans.com/snap/v1/transactions' : 'https://app.sandbox.midtrans.com/snap/v1/transactions'; 
 
 if (!strpos($_SERVER['REQUEST_URI'], '/charge')) {
 	http_response_code(404);
